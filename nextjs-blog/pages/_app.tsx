72b153270547a4b518f243f716b9a4bf7f198821
internship311 <techtosh588@gmail.com>
2023-02-26 20:32:20 +0530

    Markdown rendered for Posts

--- nextjs-blog/pages/_app.tsx
+++ nextjs-blog/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import Layout from '@/components/layout/layout';
+
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
